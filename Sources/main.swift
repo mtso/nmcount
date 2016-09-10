8d9b949c7fdcfeeeb5a2a9c75e96c8b27cd065b1
mtso <mtso@mail.com>
2016-09-09 23:22:54 -0700

    Update primadonna code

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,50 +1,51 @@
 
 import Foundation
 
-//let dataPath = "/Users/matthewtso/git/swift/card-counter/data-avatars"
-//
-//
-//let fileManager = FileManager.default
-//let enumerator = fileManager.enumerator(atPath: dataPath)
+let dataPath = "/Users/matthewtso/git/swift/card-counter/data-avatars"
+
+
+let fileManager = FileManager.default
+let enumerator = fileManager.enumerator(atPath: dataPath)
 
 
 func totalCards(atPath cardDataPath: String) -> Int? {
     
     var total = 0
     
-    let basePath = "/Users/matthewtso/git/swift/card-counter"
+    let dataPath = "/Users/matthewtso/git/neonmob-data/"
     
-    guard let stream = StreamReader(path: basePath + cardDataPath) else { return nil }
-    // "/160909-drak-the-unfathomable.txt")!
+    guard let stream = StreamReader(path: dataPath + cardDataPath) else { return nil }
+
     
-    /*
-     // Primadonna Format:
-     while(!stream.atEof) {
-     guard let line = stream.nextLine() else { continue }
-     for component in line.components(separatedBy: " ")
-                          .filter({ $0.range(of: "×") != nil }) {
-         total += Int(component.replacingOccurrences(of: "×", with: ""))!
-         }
-     }
-     */
     
-    while(!stream.atEof) {
-        
-        guard let line = stream.nextLine() else { continue }
-        
-        let components = line.components(separatedBy: " ")
-        for component in components {
-            
-            guard component.range(of:"×") != nil else { continue }
-            
-            let integer = component.replacingOccurrences(of: "×", with: "")
-            if let count = Int(integer) {
-                total += count
-            }
+    // Primadonna Format:
+    while let line = stream.nextLine() as String! {
+        for component in line.components(separatedBy: " ")
+                             .filter({ $0.range(of: "×") != nil }) {
+            total += Int(component.replacingOccurrences(of: "×", with: ""))!
         }
     }
+    
+    
+//    while(!stream.atEof) {
+//        
+//        guard let line = stream.nextLine() else { continue }
+//        
+//        let components = line.components(separatedBy: " ")
+//        for component in components {
+//            
+//            guard component.range(of:"×") != nil else { continue }
+//            
+//            let integer = component.replacingOccurrences(of: "×", with: "")
+//            if let count = Int(integer) {
+//                total += count
+//            }
+//        }
+//    }
     
     return total
 }
 
-print(totalCards(atPath: "/160909-drak-the-unfathomable.txt")!)
+print(totalCards(atPath: "/data-avatars/common/160909-drak-the-unfathomable.txt")!)
+
+
